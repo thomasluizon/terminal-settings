4a9b5adc71346678bc88059bf872918260739750
thomasluizon <thomaslrgregorio@gmail.com>
2023-10-12 00:11:17 -0300

    changed theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config 'C:\Users\thoma\AppData\Local\Programs\oh-my-posh\themes\catppuccin.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\thoma\AppData\Local\Programs\oh-my-posh\themes\clean-detailed.omp.json' | Invoke-Expression
 
 Import-Module Terminal-Icons
 Import-Module PSFzf
@@ -34,4 +34,14 @@ Set-Alias rm Remove-Item
 function which ($command) {
 	Get-Command -Name $command -ErrorAction SilentlyContinue |
 		Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
